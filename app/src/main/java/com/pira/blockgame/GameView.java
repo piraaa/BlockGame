991602ff09dd3b4ec58309cea6601dbd17a40ff1
pira <pira.y1121@gmail.com>
2020-06-23 14:48:36 +0900

    update:衝突時とゲーム終了時にToneGeneratorで音を再生する

--- app/src/main/java/com/pira/blockgame/GameView.java
+++ app/src/main/java/com/pira/blockgame/GameView.java
@@ -6,6 +6,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.SurfaceTexture;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Handler;
@@ -88,20 +90,30 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
             @Override
             public void run() {
                 Paint paint = new Paint();
+                ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_MUSIC, ToneGenerator.MAX_VOLUME);
+
                 float padLeft, padRight;
                 float ballTop, ballLeft, ballBottom, ballRight;
                 Block tmp;
                 float padTop, ballSpeedY;
                 boolean isCollision;
 
-                while (true){
+                // ブロック衝突音関係
+                int collisionTime = 0;
+                int soundIndex = ToneGenerator.TONE_DTMF_0; // 0
+
+                while (true) {
                     long startTime = System.currentTimeMillis();
 
-                    synchronized (GameView.this){
-                        if (!mIsRunnable){ break; }
+                    synchronized (GameView.this) {
+                        if (!mIsRunnable) {
+                            break;
+                        }
 
                         Canvas canvas = lockCanvas();
-                        if(canvas == null){ continue; }
+                        if (canvas == null) {
+                            continue;
+                        }
 
                         canvas.drawColor(Color.BLACK);
 
@@ -117,24 +129,27 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
                         ballRight = mBall.getX() + mBallRadius;
 
                         // 壁衝突判定
-                        if((ballLeft<0 && mBall.getSpeedX()<0) || (ballRight>=getWidth() && mBall.getSpeedX()>0)){
-                            mBall.setSpeedX(-1*mBall.getSpeedX());
+                        if ((ballLeft < 0 && mBall.getSpeedX() < 0) || (ballRight >= getWidth() && mBall.getSpeedX() > 0)) {
+                            mBall.setSpeedX(-1 * mBall.getSpeedX());
+                            tg.startTone(ToneGenerator.TONE_DTMF_0, 10); // 第2引数=再生時間 10[ms]
                         }
-                        if(ballTop<0){
-                            mBall.setSpeedY(-1*mBall.getSpeedY());
+                        if (ballTop < 0) {
+                            mBall.setSpeedY(-1 * mBall.getSpeedY());
+                            tg.startTone(ToneGenerator.TONE_DTMF_0, 10); // 第2引数=再生時間 10[ms]
                         }
 
                         // 死亡判定
-                        if(ballTop > getHeight()){
-                            if(mLife>0){
+                        if (ballTop > getHeight()) {
+                            if (mLife > 0) {
                                 mLife--;
                                 try {
                                     Thread.sleep(1000);
-                                }catch (InterruptedException e){
+                                } catch (InterruptedException e) {
 
                                 }
                                 mBall.reset();
-                            }else{
+                            } else {
+                                tg.startTone(ToneGenerator.TONE_CDMA_ALERT_NETWORK_LITE);
                                 unlockCanvasAndPost(canvas);
                                 Message message = Message.obtain();
                                 Bundle bundle = new Bundle();
@@ -150,43 +165,59 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
                         // ブロック衝突判定
                         isCollision = false;
                         tmp = getBlock(ballLeft, mBall.getY());
-                        if(tmp != null){
+                        if (tmp != null) {
                             mBall.setSpeedX(-1 * mBall.getSpeedX());
                             tmp.collision();
                             isCollision = true;
                         }
                         tmp = getBlock(mBall.getX(), ballTop);
-                        if(tmp != null){
+                        if (tmp != null) {
                             mBall.setSpeedY(-1 * mBall.getSpeedY());
                             tmp.collision();
                             isCollision = true;
                         }
                         tmp = getBlock(ballRight, mBall.getY());
-                        if(tmp != null){
+                        if (tmp != null) {
                             mBall.setSpeedX(-1 * mBall.getSpeedX());
                             tmp.collision();
                             isCollision = true;
                         }
                         tmp = getBlock(mBall.getX(), ballBottom);
-                        if(tmp != null){
+                        if (tmp != null) {
                             mBall.setSpeedY(-1 * mBall.getSpeedY());
                             tmp.collision();
                             isCollision = true;
                         }
 
+                        // ブロック衝突時の音
+                        if(isCollision){
+                            if(collisionTime>0){
+                                if(soundIndex<ToneGenerator.TONE_DTMF_D) { // 15
+                                    soundIndex++;
+                                }
+                            }else{
+                                soundIndex=ToneGenerator.TONE_DTMF_0;
+                            }
+                            collisionTime = 10;
+                            tg.startTone(soundIndex, 10);
+                        }else if(collisionTime>0){
+                            collisionTime--;
+                        }
+
                         // パッド衝突判定
                         padTop = mPad.getTop();
                         ballSpeedY = mBall.getSpeedY();
-                        if(ballBottom > padTop && (ballBottom-ballSpeedY) < padTop && padLeft < ballRight && padRight > ballLeft) {
+                        if (ballBottom > padTop && (ballBottom - ballSpeedY) < padTop && padLeft < ballRight && padRight > ballLeft) {
                             if (ballSpeedY < mBlockHeight / 3) {
                                 ballSpeedY = -1 * ballSpeedY * 1.05f;
                             } else {
                                 ballSpeedY = -1 * ballSpeedY;
                             }
                             mBall.setSpeedY(ballSpeedY);
+                            tg.startTone(ToneGenerator.TONE_DTMF_0, 10); // 第2引数=再生時間 10[ms]
                         }
 
-                        for(DrawableItem item : mItemList){
+                        for (DrawableItem item : mItemList) {
                             item.draw(canvas, paint);
 
                         }
@@ -197,7 +228,8 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
 
                         unlockCanvasAndPost(canvas);
 
-                        if(isCollision && getBlockCount()==0){
+                        if (isCollision && getBlockCount() == 0) {
+                            tg.startTone(ToneGenerator.TONE_CDMA_ALERT_NETWORK_LITE);
                             Message message = Message.obtain();
                             Bundle bundle = new Bundle();
                             bundle.putBoolean(ClearActivity.EXTRA_IS_CLEAR, true);
@@ -208,15 +240,16 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
                         }
 
                         long sleepTime = 16 - System.currentTimeMillis() - startTime; // 16[ms] = 1/60[s]
-                        if(sleepTime>0){
+                        if (sleepTime > 0) {
                             try {
                                 Thread.sleep(sleepTime);
-                            }catch (InterruptedException e){
+                            } catch (InterruptedException e) {
 
                             }
                         }
                     }
                 }
+                tg.release();
             }
         });
         mIsRunnable = true;
@@ -252,7 +285,7 @@ public class GameView extends TextureView implements TextureView.SurfaceTextureL
 
         mPad = new Pad(height*0.8f, height*0.82f);
         mItemList.add(mPad);
-        mPadHalfWidth = width / 7;
+        mPadHalfWidth = width / 7; // 画面サイズを基準にパッドの幅を決定
 
         mBallRadius = width<height ? width/40 : height/40;
         mBall = new Ball(mBallRadius, width/2, height/2);
